a8499d056a4462e04afef168057377eb672e61e0
Faiz <faizkhan8225@gmail.com>
2024-08-05 19:10:05 +0530

    Able to store post in database.

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -1,5 +1,7 @@
 const {validationResult} = require("express-validator");
 
+const Post = require("../models/post")
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -28,15 +30,20 @@ exports.createPost = (req, res, next) => {
 
   const title = req.body.title;
   const content = req.body.content;
+  
   // Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: "Faiz" },
-      createdAt: new Date(),
-    },
-  });
+  const post = new Post({
+    title: title,
+    content: content,
+    creator: { name: "Faiz Khan" },
+    imageUrl: 'images/pic.jpg'
+  })
+
+  post.save().then((result) => {
+    // console.log(result);
+    res.json({
+      message: "Post created successfully.",
+      post: result
+    })
+  }).catch(err => console.log(err))
 };
