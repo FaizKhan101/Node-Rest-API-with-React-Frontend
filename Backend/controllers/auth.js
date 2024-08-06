9663f3d4ea2681131d1c112694a50dd0b2ef32f4
Faiz <faizkhan8225@gmail.com>
2024-08-06 18:15:43 +0530

    Signing user up.

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
 
@@ -15,4 +16,25 @@ exports.signup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const name = req.body.name;
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPass) => {
+      const user = new User({
+        email: email,
+        password: hashedPass,
+        name: name,
+      });
+
+      return user.save();
+    })
+    .then((result) => {
+      res.status("201").json({ message: "User created", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.sratusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
