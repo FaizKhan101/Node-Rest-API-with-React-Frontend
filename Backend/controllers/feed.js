efd3f528f22129b8cfd4b76b112963adde449e7e
Faiz <faizkhan8225@gmail.com>
2024-08-06 11:20:51 +0530

    Serve images statically and add error handling middleware.

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -1,49 +1,51 @@
-const {validationResult} = require("express-validator");
+const { validationResult } = require('express-validator');
 
-const Post = require("../models/post")
+const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "images/duck.jpg",
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/duck.jpg',
         creator: {
-          name: "Maximilian",
+          name: 'Maximilian'
         },
-        createdAt: new Date(),
-      },
-    ],
+        createdAt: new Date()
+      }
+    ]
   });
 };
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
-
-  if (!errors.isEmpty) {
-    return res.json({
-      message: "Validation failed, entered data is incorrect.",
-    });
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
   }
-
   const title = req.body.title;
   const content = req.body.content;
-  
-  // Create post in db
   const post = new Post({
     title: title,
     content: content,
-    creator: { name: "Faiz Khan" },
-    imageUrl: 'images/pic.jpg'
-  })
-
-  post.save().then((result) => {
-    // console.log(result);
-    res.json({
-      message: "Post created successfully.",
-      post: result
+    imageUrl: 'images/duck.jpg',
+    creator: { name: 'Maximilian' }
+  });
+  post
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
     })
-  }).catch(err => console.log(err))
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
